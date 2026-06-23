6f1739dc379731dc3ff1edd34dcda93cdc61743d
tahiri-amine <01aminetahiri01@gmail.com>
2026-06-24 00:09:10 +0100

    refactor : replace sys.argv with argparse &  add preview subcommand

--- main.py
+++ main.py
@@ -1,34 +1,40 @@
-#import nedded Modules 
-import sys
-from organiser import FileOrganiser
+import argparse
+from organiser import FileOrganiser 
 from logger import MoveLogger
-l = sys.argv # [1] = the mode ; [2] = path; [0] = idk
-#catch the mode and chose the correct function
-
-logger = MoveLogger()
-if len(l) < 2:
-    print("Usage: py main.py <mode> [path] [args]")
-    sys.exit()
-
-if l[1] == "organize":
-    organiser = FileOrganiser(l[2])
-    organiser.organiser()
-elif l[1] == "find":
-    organiser = FileOrganiser(l[2])
-    result = organiser.find_files(l[3])
-    for file in result:
-        print(file)
-elif l[1] == "undo":
-    logger.undo_last()
-elif l[1] == "history":
-    data = logger.get_history()
-    if not data:
-        print("No history yet.")
+parser = argparse.ArgumentParser(description="File Organizer")
+sub_parser = parser.add_subparsers(dest ="command")
+organize = sub_parser.add_parser("organize")
+find = sub_parser.add_parser("find")
+undo = sub_parser.add_parser("undo")
+history = sub_parser.add_parser("history")
+organize.add_argument("-fp","--folder_path",required=True)
+find.add_argument("-fp","--folder_path",required=True)
+find.add_argument("-ext",required=True)
+preview = sub_parser.add_parser("preview")
+preview.add_argument("-fp","--folder_path",required=True)
+args = parser.parse_args()
+if args.command == "organize":
+    obj = FileOrganiser(args.folder_path)# question about args.fp why? 
+    obj.organiser()
+elif args.command == "find":
+    obj = FileOrganiser(args.folder_path)
+    founded_files = obj.find_files(args.ext)
+    if not  founded_files:
+        print(f"not file found with {args.ext}:(\n make sure you wrote .{args.ext} ")
     else:
-        print("__"*30, "your History:", "__"*30)
-        for element in data:
-            src, dist = element.split("->")
-            print(f"{src} --------> {dist}")
-else:
-    print("Unknown mode. Check README.md")
-    sys.exit()
+        print(f"the found files are : \n{founded_files}")
+elif args.command =="undo":
+    obj = MoveLogger()
+    obj.undo()
+elif args.command == "history":
+    obj = MoveLogger()
+    obj.get_history()
+elif args.command == "preview":
+    obj = FileOrganiser(args.folder_path)
+    obj.prevew()
+
+
+
+
+
+
